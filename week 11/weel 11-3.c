10e4ce1ebd30dc2a66d59d1ee6b1b7c4ce3d84af
Evan00405 <91250140+Evan00405@users.noreply.github.com>
2022-05-12 23:18:18 +0800

    Add files via upload

---
+++ week 11/weel 11-3.c
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class students{
+ public:
+    char name[30];
+    int grade;
+};
+int main()
+{
+    int N;
+    cin >> N;
+    vector<student> stu(N);
+    for(int i=0; i<N; i++){
+        cin >> stu[i].name >> stu[i].grade;
+    }
+
+    for(int i=0; i<N; i++){
+        cout << stu[i].name << " " << stu[i].grade << endl;
+    }
+
+}
